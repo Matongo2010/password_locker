90869afcd0224f3873192f8a0fa56f27a1cdd9c6
Matongo2010 <justineadriano638@gmail.com>
2019-07-20 15:27:50 +0300

    Add a docstring to my method

--- password.py
+++ password.py
@@ -7,3 +7,7 @@ class sign-up:
     pass
 
     def __init__ (self, first_name, last_name, phone_number, username)
+
+    '''
+        __init__ method that helps us define properties for our objects.
+    '''
