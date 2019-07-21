3a1ba65c0aeed407769f05d2ad24898cfd34235d
Matongo2010 <justineadriano638@gmail.com>
2019-07-21 14:32:41 +0300

    Add a setup method in the test class

--- password_test.py
+++ password_test.py
@@ -9,3 +9,9 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+
+         '''
+        Set up method to run before each test cases.
+        '''
