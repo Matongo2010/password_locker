b110185c365aa74a25fd21b5960fab649ea2b81e
Matongo2010 <justineadriano638@gmail.com>
2019-07-22 10:04:46 +0300

    create a method to assign credential lists

--- credentials.py
+++ credentials.py
@@ -1,2 +1,16 @@
 import pyperclip
 class Credentials:
+
+    '''
+    class that creates instaces of user accounts
+    '''
+
+    credentials_list = []
+
+    #assign property to credential list#
+
+    def __init__(self, account , email , passlock):
+    
+        self.account = account
+        self.email = email
+        self.password = password
