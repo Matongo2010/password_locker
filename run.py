cc1314c431f1e68f80a947de2cdb34e7f134360f
Matongo2010 <justineadriano638@gmail.com>
2019-07-22 11:08:52 +0300

    create a method to search user

--- run.py
+++ run.py
@@ -10,3 +10,18 @@ def create_useraccount(username, password):
     '''
     new_user = User(username, password)
     return new_user
+
+    #save user#
+def save_user(user):
+    '''
+    method save user  account
+    '''
+    user.save_user()
+
+     #search user#
+
+def find_user(username):
+    '''
+    method for find user using username
+    '''
+    return User.find_user(username)
