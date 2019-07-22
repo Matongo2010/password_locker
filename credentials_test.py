d64a6ceaba3c5f64af7cc5db5e08d240f05e95f3
Matongo2010 <justineadriano638@gmail.com>
2019-07-22 10:34:17 +0300

    create a setup method

--- credentials_test.py
+++ credentials_test.py
@@ -3,3 +3,9 @@ from credentials import Credentials
 import pyperclip
 
 class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        setup before a test is run
+        '''
+        self.new_credential = Credentials("facebook", "justineadriano@gmail", "matongo2010")
